3194532d758d684ecc7f3e39abe1c191a03f8cb9
JamesDaniel <jamesmcgarr@protonmail.com>
2020-12-31 01:32:15 +0000

    I am so stupid

--- twilio-crazy-promises/twilio-as-promise.js
+++ twilio-crazy-promises/twilio-as-promise.js
@@ -3,20 +3,12 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 function twilioPromise() {
-  return new Promise((resolve, reject) => {
-    client.messages
-      .create({
-        body: 'This is the ship... Blah.',
-        from: process.env.PHONE_FROM,
-        to: process.env.PHONE_TO
-      })
-      .then(message => {
-        resolve(message.from);
-      })
-      .catch(err => {
-        reject(err);
-      });
-  });
+  return client.messages
+    .create({
+      body: 'This is the ship... Blah.',
+      from: process.env.PHONE_FROM,
+      to: process.env.PHONE_TO
+    });
 }
 
 function sendTwilio() {
